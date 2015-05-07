130cd3ef4392911404ac1568272e4382a4828657
balazs dianiska <balazs.dianiska@gmail.com>
2015-05-07 16:19:45 +0200

    auto commit (mixture)

--- js/app.js
+++ js/app.js
@@ -5,10 +5,10 @@ $(document).foundation({
 	"magellan-expedition": {
 	  active_class: 'active', // specify the class used for active sections
 	  threshold: 0, // how many pixels until the magellan bar sticks, 0 = auto
-	  destination_threshold: 80, // pixels from the top of destination for it to be considered active
-	  throttle_delay: 50, // calculation throttling to increase framerate
-	  fixed_top: 80, // top distance in pixels assigend to the fixed element on scroll
-	  offset_by_height: true // whether to offset the destination by the expedition height. Usually you want this to be true, unless your expedition is on the side.
+	  destination_threshold: 0, // pixels from the top of destination for it to be considered active
+	  throttle_delay: 30, // calculation throttling to increase framerate
+	  fixed_top: 80 // top distance in pixels assigend to the fixed element on scroll
+	  // offset_by_height: false // whether to offset the destination by the expedition height. Usually you want this to be true, unless your expedition is on the side.
 	}
 });
 
